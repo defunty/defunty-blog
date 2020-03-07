e11bbcb1782bcf53f7d36e833b3cbbdeecd35a3f
defunty <tkqrt4131@gmail.com>
2020-03-08 02:23:21 +0900

    [Fix] fix

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="content">
     <Post title="週報（2020/02/28）" createdDate="2020/02/28" :body="body_20200229"></Post>
-    <!-- <Post title="週報（2020/03/07）" createdDate="2020/03/07" :body="body_20200307"></Post> -->
+    <Post title="週報（2020/03/08）" createdDate="2020/03/08" :body="body_20200308"></Post>
     <!-- <Post title="ブログ工事計画書" createdDate="2020/03/07" :body="body_202003xx"></Post> -->
   </div>
 </template>
@@ -16,12 +16,11 @@ import Post from './Post.vue'
 import post_20200229 from '../post_contents/20200229.js'
 import post_20200301 from '../post_contents/20200301.js'
 import post_20200308 from '../post_contents/20200308.js'
-import post_202003xx from '../post_contents/202003xx.js'
 
 const body_20200229 = post_20200229.html()
 const body_20200301 = post_20200301.html()
 const body_20200308 = post_20200308.html()
-const body_202003xx = post_202003xx.html()
+
 export default {
   components: {
     Post
@@ -30,8 +29,7 @@ export default {
     return {
       body_20200229: body_20200229,
       body_20200301: body_20200301,
-      body_20200308: body_20200308,
-      body_202003xx: body_202003xx
+      body_20200308: body_20200308
     }
   }
 }

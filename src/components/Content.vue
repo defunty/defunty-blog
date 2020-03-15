1dc76b8b87728955475f5e0d8ff5bdf54e4eb0b3
defunty <tkqrt4131@gmail.com>
2020-03-16 01:47:59 +0900

    [Add] post

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="content">
+    <Post title="週報（2020/03/16）" createdDate="2020/03/16" :body="body_20200316"></Post>
     <Post title="topコマンドを理解する" createdDate="2020/03/09" :body="body_20200309"></Post>
     <Post title="週報（2020/03/08）" createdDate="2020/03/08" :body="body_20200308"></Post>
     <Post title="週報（2020/02/28）" createdDate="2020/02/28" :body="body_20200229"></Post>
@@ -17,10 +18,12 @@ import Post from './Post.vue'
 import post_20200229 from '../post_contents/20200229.js'
 import post_20200308 from '../post_contents/20200308.js'
 import post_20200309 from '../post_contents/20200309.js'
+import post_20200316 from '../post_contents/20200316.js'
 
 const body_20200229 = post_20200229.html()
 const body_20200308 = post_20200308.html()
 const body_20200309 = post_20200309.html()
+const body_20200316 = post_20200316.html()
 
 export default {
   components: {
@@ -30,7 +33,8 @@ export default {
     return {
       body_20200229: body_20200229,
       body_20200308: body_20200308,
-      body_20200309: body_20200309
+      body_20200309: body_20200309,
+      body_20200316: body_20200316
     }
   }
 }

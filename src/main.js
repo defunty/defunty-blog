df9e0b53eb1c993d0b3e906d8c5b2aaad6356a69
defunty <tkqrt4131@gmail.com>
2020-03-22 15:07:47 +0900

    [Add] vue-router

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const routes = [
+]
+
+const router = new VueRouter({
+  mode:'history',
+  routes
+})
 
 // Google Analytics
 Vue.use(VueAnalytics, {
   id: 'UA-108454395-5',
   router
 })
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
